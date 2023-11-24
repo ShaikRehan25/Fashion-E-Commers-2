5b4cb5aa538c9474af45908027c5d795b1326a18
ShaikRehan25 <rehanshaik371@gmail.com>
2023-11-24 19:12:49 +0530

    offers file is added

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 function Shop() {
   return (
@@ -8,6 +9,7 @@ function Shop() {
         <div className='shop'>
             <Hero/>
             <Popular/>
+            <Offers/>
         </div>
     </>
   )
