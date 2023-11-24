5a88d006dd5cf76001c8d7eedf71ef0cb4ad70a1
ShaikRehan25 <rehanshaik371@gmail.com>
2023-11-24 16:22:43 +0530

    space add

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
             <Route path=':productId' element={<Product/>}/>
           </Route>
           <Route path='/cart' element={<Cart/>}/>
+          
           <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
         </BrowserRouter>
