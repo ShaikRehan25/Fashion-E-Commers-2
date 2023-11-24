fb2617f0fab9d5d9d06bc48ecd6873f74f4ab5f9
ShaikRehan25 <rehanshaik371@gmail.com>
2023-11-24 22:55:26 +0530

    adding products

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>}></Route>
-          <Route path='/mens' element={<ShopCategory category='men'/>}/>
-          <Route path='/womens' element={<ShopCategory category='women'/>}/>
-          <Route path='/kids' element={<ShopCategory category='kid'/>}/>
+          <Route path='/mens' element={<ShopCategory banner = 'https://img.freepik.com/premium-psd/fashion-sale-social-media-facebook-cover-design-template_169307-1928.jpg?w=2000' category='men'/>}/>
+          <Route path='/womens' element={<ShopCategory banner = 'https://ttascott.in/uploads/slider/slider02.jpg' category='women'/>}/>
+          <Route path='/kids' element={<ShopCategory banner = 'https://stylehub.shoppersstop.com/wp-content/uploads/2021/08/kidsBuyingGuide.jpg' category='kid'/>}/>
           <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
--- src/Pages/ShopCategory.js
+++ src/Pages/ShopCategory.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext';
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import Item from '../Components/Items/Item';
 
-function ShopCategory() {
+
+function ShopCategory(props) {
+    const {all_products} = useContext(ShopContext);
   return (
     <>
-        <div className='shopcategory'>
-
+        <div className='shop-category'>
+            <img className='shopcategory-banner' src={props.banner} alt="" />
+            <div className="shopcategory-indexsort">
+                <p>
+                    <span>Showing 1-12</span> out of 36 products
+                </p>
+                <div className="shopcategory-sort">
+                    Sort by <IoIosArrowDropdownCircle className='sort-icon'/>
+                </div>
+            </div>
+            <div className="shopcategory-products">
+                {all_products.map((item,i)=>{
+                    if(props.category===item.category)
+                    {
+                        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    }else{
+                        return null;
+                    }
+                })}
+            </div>
+            <div className="shopcategory-loadmore">
+                Explore More
+            </div>
         </div>
     </>
   )
