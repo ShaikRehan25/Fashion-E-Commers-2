36496f843fedda3d129081e8e4784660a30dea55
ShaikRehan25 <rehanshaik371@gmail.com>
2023-11-24 17:37:45 +0530

    items date is added

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,34 @@
+
+
+const data_product = [
+    {
+        id:1,
+        name:'Women Viscose Rayon Kurta',
+        image:'https://rukminim2.flixcart.com/image/612/612/xif0q/ethnic-set/j/h/p/s-131-wine-kurta-pant-dupatta-set-ziva-fashion-original-imagu2p8jgggzh75.jpeg?q=70',
+        new_price:799,
+        old_price:2999,
+    },
+    {
+        id:2,
+        name:'Women Viscose Rayon Kurta',
+        image:'https://rukminim2.flixcart.com/image/612/612/l0o6nbk0/ethnic-set/z/9/i/s-ssf-k29-shree-shital-print-original-imagce2aztmkeh6h.jpeg?q=70',
+        new_price:654,
+        old_price:2999,
+    },
+    {
+        id:3,
+        name:'Women Viscose Rayon Kurta',
+        image:'https://rukminim2.flixcart.com/image/612/612/xif0q/ethnic-set/t/t/p/s-akkurti-ewedoos-original-imagqbqcc9hh5aez.jpeg?q=70',
+        new_price:559,
+        old_price:2499,
+    },
+    {
+        id:4,
+        name:'Women Viscose Rayon Kurta',
+        image:'https://rukminim2.flixcart.com/image/612/612/xif0q/ethnic-set/e/h/g/xl-gown-barters-boutique-original-imague9fhenre7hh.jpeg?q=70',
+        new_price:699,
+        old_price:1999,
+    }
+];
+
+export default data_product;
--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
 
 function Shop() {
   return (
     <>
         <div className='shop'>
             <Hero/>
+            <Popular/>
         </div>
     </>
   )
